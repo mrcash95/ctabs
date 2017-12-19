c211f2134608555aebe74309114b8d98049e4c48
mrcash95 <mrcashphc@gmail.com>
2017-12-19 10:13:32 +0800

    Add files via upload

--- popup.js
+++ popup.js
@@ -13,8 +13,8 @@ function loadBody() {
 			var img = document.createElement("img");
 			img.src = result.getCombinedDataUrl();
 			//TODO make dynamic compared to window size?
-			img.width = 600; //default 300
-			img.height = 400; //default 200
+			img.width = 300;
+			img.height = 200;
 			body.appendChild(img);
 		});
 	});
